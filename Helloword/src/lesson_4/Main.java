68e99950ffd7337f10953e0bdf28060830d92b6f
nhatln <nguyenlongnhat1609@gmail.com>
2023-03-09 14:23:27 +0700

    1

---
+++ Helloword/src/lesson_4/Main.java
@@ -0,0 +1,13 @@
+package lesson_4;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hw2wweello world!");
+        Oto honda = new Oto();
+        honda.hangOto = "honda";
+        honda.bienSo = "1999";
+        honda.namSx = 2008;
+        honda.run();
+    }
+}
+
---
+++ Helloword/src/lesson_4/Oto.java
@@ -0,0 +1,12 @@
+package lesson_4;
+
+public class Oto {
+    String hangOto;
+    String bienSo;
+    int namSx;
+
+    public  void  run()
+    {
+        System.out.println("xe" + hangOto + "bien so" + bienSo + " nam san xuat" + namSx);
+    }
+}
